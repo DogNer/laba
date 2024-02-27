9de85ad454a6f31349a0663d4a0c4c4d474171bf
Dana <danamusenko780@gmail.com>
2024-02-28 01:09:41 +0200

    вывод

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import Models.ArrList;
+import Tables.FacultyTab;
 import University.Faculty;
+
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -6,7 +10,14 @@ public class Main {
     public static void main(String[] args) {
 
         Faculty fac = new Faculty();
-        fac.addFaculty(new Faculty("fhj", "sdf"));
+        fac.addFaculty(new Faculty("FEN", "GLIBA"));
+        fac.addFaculty(new Faculty("FI", "Neznau"));
         System.out.println(fac);
+
+        FacultyTab tab = new FacultyTab(fac.getArrFaculty());
+        List<String> names = new ArrList<>();
+        names.add("id");
+        names.add("Name");
+        tab.getTable(names);
     }
 }
